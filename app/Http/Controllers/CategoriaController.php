adf8149bd404f0b832320c3dda3ffafb0ff3bd38
Rafael Santana <santana_r@outlook.com>
2025-06-13 15:54:39 -0300

    feat: implementado CRUD completo de categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
@@ -11,7 +12,8 @@ class CategoriaController extends Controller
      */
     public function index()
     {
-        //
+        // retorna todas as categorias da base de dados
+        return response()->json(Categoria::all(), 200);
     }
 
     /**
@@ -19,7 +21,31 @@ class CategoriaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validar a solicitação
+        $request->validate(
+            [
+                'nome' => 'required|string|min:2|max:100',
+                'ativo' => 'boolean',
+            ],
+            [
+                'nome.required' => 'O nome da categoria é obrigatório.',
+                'nome.string' => 'O nome da categoria deve ser um texto válido.',
+                'nome.min' => 'O nome da categoria deve ter no mínimo 2 caracteres.', 
+                'nome.max' => 'O nome da categoria não pode ter mais que 100 caracteres.', 
+
+                'ativo.boolean' => 'O campo "ativo" deve ser verdadeiro ou falso (true ou false).',
+            ]
+        );
+
+        // Add nova Categoria na base de dados
+        $categoria = Categoria::create($request->all());
+
+        return response()->json(
+            [
+                'message' => 'Categoria cadastrada com sucesso!',
+                'data' => $categoria,
+            ], 200
+        );
     }
 
     /**
@@ -27,7 +53,16 @@ class CategoriaController extends Controller
      */
     public function show(string $id)
     {
-        //
+        // Exibir uma Categoria em especifico
+        $categoria = Categoria::Find($id);
+
+        if($categoria) {
+            return response()->json($categoria, 200);
+        } else {
+            return response()->json([
+                'message' => 'Categoria não encontrada'
+            ], 404);
+        }
     }
 
     /**
@@ -35,7 +70,36 @@ class CategoriaController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // Validar a solicitação
+        $request->validate(
+            [
+                'nome' => 'required|string|max:255',
+                'ativo' => 'boolean',
+            ],
+            [
+                'nome.required' => 'O nome da categoria é obrigatório.',
+                'nome.string' => 'O nome da categoria deve ser um texto válido.',
+                'nome.max' => 'O nome da categoria não pode ter mais que 255 caracteres.', 
+
+                'ativo.boolean' => 'O campo "ativo" deve ser verdadeiro ou falso (true ou false).',
+            ]
+        );
+
+         // Atualizar uma Categoria
+        $categoria = Categoria::Find($id);
+
+        if($categoria) {
+            $categoria->update($request->all());
+            return response()->json([
+                'message' => 'Categoria atualizada com sucesso!',
+                'data' => $categoria
+            ], 200);
+        } else {
+             return response()->json([
+                'message' => 'Categoria não encontrada'
+            ], 404);
+        }
+
     }
 
     /**
@@ -43,6 +107,18 @@ class CategoriaController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+         // Deletar uma Categoria
+        $categoria = Categoria::Find($id);
+
+        if($categoria) {
+            $categoria->delete();
+            return response()->json([
+                'message' => 'Categoria deletada com sucesso!',
+            ], 200);
+        } else {
+             return response()->json([
+                'message' => 'Categoria não encontrada'
+            ], 404);
+        }
     }
 }
