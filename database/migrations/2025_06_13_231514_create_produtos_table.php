b0461fff5680fd3588330238947e3dcd340fcf4b
Rafael Santana <santana_r@outlook.com>
2025-06-13 20:24:16 -0300

    feat: criar tabela de produtos

---
+++ database/migrations/2025_06_13_231514_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->text('descricao')->nullable();
+            $table->decimal('preco', 8, 2);
+            $table->foreignId('categoria_id')
+                  ->constrained('categorias')
+                  ->onDelete('restrict');
+            $table->string('imagem')->nullable();
+            $table->boolean('ativo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
