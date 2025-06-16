8b5ab3decc34e5411196c8ee7d6a6feb22d82fea
Rafael Santana <santana_r@outlook.com>
2025-06-16 17:33:28 -0300

    feat(produtos): CRUD completo implementado

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -93,7 +93,58 @@ class ProdutoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // Validar o produto Atualizado
+        $validacao = $request->validate(
+            [
+                'nome' => 'required|string|min:3|max:100',
+                'descricao' => 'nullable|string|max:1000',
+                'preco' => 'required|numeric|min:0.01',
+                'categoria_id' => 'required|exists:categorias,id',
+                'imagem' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+                'ativo' => 'required|boolean',
+            ],
+            [
+                // nome
+                'nome.required' => 'O nome do produto é obrigatório.',
+                'nome.string' => 'O nome do produto deve ser um texto válido.',
+                'nome.min' => 'O nome do produto deve ter no mínimo 3 caracteres.', 
+                'nome.max' => 'O nome do produto não pode ter mais que 100 caracteres.', 
+                
+                // descricao
+                'descricao.string' => 'A descrição do produto deve ser um texto válido.',
+                'descricao.max' => 'A descrição do produto não pode ter mais que 1000 caracteres.',
+
+                // preco
+                'preco.required' => 'O preço do produto é obrigatório.',
+                'preco.numeric' => 'O preço deve ser um número válido.',
+                'preco.min' => 'O preço deve ser maior que zero.',
+
+                // categoria_id
+                'categoria_id.required' => 'A categoria é obrigatória.',
+                'categoria_id.exists' => 'A categoria selecionada não existe.',
+
+                // imagem
+                'imagem.image' => 'O arquivo deve ser uma imagem válida.',
+                'imagem.mimes' => 'A imagem deve estar no formato: jpg, jpeg ou png.',
+                'imagem.max' => 'A imagem não pode ter mais que 2MB.',
+
+                // ativo
+                'ativo.required' => 'O campo "ativo" é obrigatório.',
+                'ativo.boolean' => 'O campo "ativo" deve ser verdadeiro ou falso (true ou false).',
+            ]
+        );
+
+        $validacao['imagem'] = $this->uploadImagem($request);
+
+        // Atualizar Produto
+        $produto = Produto::Find($id);
+
+        if($produto) {
+            $produto->update($validacao);
+            return ApiResponse::sucesso($produto, 'Produto Atualizado com sucesso');
+        } else {
+             return ApiResponse::erro('Produto não encontrado');
+        }
     }
 
     /**
