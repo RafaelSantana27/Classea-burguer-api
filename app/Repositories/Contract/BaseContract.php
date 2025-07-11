f855d8481f4d691e9e767a93433c367f75ec7c8f
Rafael Santana <santana_r@outlook.com>
2025-07-11 14:49:07 -0300

    feat: Criando BaseContract para definição dos métodos CRUD

---
+++ app/Repositories/Contract/BaseContract.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Contract;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseContract
+{
+    public function all():Collection;
+    public function find(int $id): ?Model;
+    public function create(array $dados): ?Model;
+    public function update(int $id, array $dados): ?Model;
+    public function delete(int $id): ?Model;
+    public function loadModel():Model;
+}
