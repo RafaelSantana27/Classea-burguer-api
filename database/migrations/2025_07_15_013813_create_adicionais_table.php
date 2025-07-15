57ad6d579c7c3be9198134cb40335ac5028861cd
Rafael Santana <santana_r@outlook.com>
2025-07-14 22:41:32 -0300

    Criado model base de dados para Adicionais

--- database/migrations/2025_07_15_013813_create_adicionais_table.php
+++ database/migrations/2025_07_15_013813_create_adicionais_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('adicionais', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->decimal('preco', 8, 2)->default(0.00);
+            $table->boolean('ativo')->default(true);
             $table->timestamps();
         });
     }
