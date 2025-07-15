18bcfa38f0a6bfd6c242765807e639e7f2151a1a
Rafael Santana <santana_r@outlook.com>
2025-07-14 22:23:33 -0300

    Ajustado Update e Delete para receber um Model

--- app/Repositories/Contract/BaseContract.php
+++ app/Repositories/Contract/BaseContract.php
@@ -10,6 +10,8 @@ interface BaseContract
     public function all():Collection;
     public function find(int $id): ?Model;
     public function create(array $dados): ?Model;
-    public function update(int $id, array $dados): ?Model;
-    public function delete(int $id): ?Model;
+    // public function update(int $id, array $dados): ?Model;
+    // public function delete(int $id): ?Model;
+    public function update(Model $model): ?Model;
+    public function delete(Model $model): ?Model;
 }
