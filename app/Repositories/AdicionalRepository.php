63ec6c7005272369bfdb936c0612d580eb9d65c2
Rafael Santana <santana_r@outlook.com>
2025-07-14 23:07:45 -0300

    Criado construtor responsavel por passar model

--- app/Repositories/AdicionalRepository.php
+++ app/Repositories/AdicionalRepository.php
@@ -6,5 +6,16 @@ use App\Models\Adicional;
 
 class AdicionalRepository extends BaseRepository
 {
-    
+    public function __construct(
+        protected Adicional $adicional
+    )
+    {
+        parent::__construct($this->adicional);
+    }
+
+    public function buscarAtivos() //Função criada para TESTE........
+    {
+        return $this->adicional->where('ativo', true)->get()
+                ?->makeHidden(['created_at', 'updated_at']);
+    }
 }
