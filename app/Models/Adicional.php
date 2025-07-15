a20938febfb7be315cc6064e28c2a3ea0a08a05d
Rafael Santana <santana_r@outlook.com>
2025-07-14 22:51:05 -0300

    Configurado fillable para proteger campos da tabela e criado função para relação com tabela produtos

--- app/Models/Adicional.php
+++ app/Models/Adicional.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Adicional extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'preco',
+        'ativo'
+    ];
+
+    public function produtos()
+    {
+        return $this->belongsToMany(Produto::class, 'adicional_produto');
+    }
 }
